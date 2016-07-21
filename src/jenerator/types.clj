7d3b6473c67deac594bee5aca172dad0bc21d568
Ruslan Dudin <rdudin@devexperts.com>
2016-07-21 19:30:31 +0300

    #7 Add annotations

--- src/jenerator/types.clj
+++ src/jenerator/types.clj
@@ -1,7 +1,11 @@
 (ns jenerator.types
   (:require [jenerator.util :as u]))
 
-(defn jenerate-class-ref [class-ref]
+(defn jenerate-class-ref
+  "Class -> String
+   Takes a class. Returns either a full name, or a simple name,
+   based on whether class is from `java.lang` package."
+  [class-ref]
   (let [package (-> class-ref .getPackage .getName)]
     (if (= package "java.lang") (.getSimpleName class-ref) (.getName class-ref))))
 
@@ -12,7 +16,12 @@
                     (jenerate-fn value))]
     (str (name arg-name) " = " value-str)))
 
-(defn jenerate-annotation [jenerate-fn {:keys [class args] :or {args {}}}]
+(defn jenerate-annotation
+  "({Key -> Any} -> String) -> {Key -> Any} -> String
+   Takes a jeneration function and an annotation AST map.
+   Uses function to jenerate all annotation arguments (if any).
+   Produces string source for Java annotation instantiation."
+  [jenerate-fn {:keys [class args] :or {args {}}}]
   {:pre [(fn? jenerate-fn) (class? class) (map? args)]}
   (let [body (str "@" (jenerate-class-ref class))]
     (if (empty? args) (str body "()")
