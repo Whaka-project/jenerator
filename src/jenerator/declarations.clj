9190a1abe956ec76a9b211142207007337a13a00
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2016-08-03 01:26:58 +0300

    #26 jenerate-var doc-string added

--- src/jenerator/declarations.clj
+++ src/jenerator/declarations.clj
@@ -2,6 +2,19 @@
   (:require [jenerator.util :as u]))
 
 (defn jenerate-var
+  "; (Any -> String) -> {Key -> Any} -> String
+   Function takes a jen function and AST-map for variable declaration.
+   Produces string Java source.
+
+   AST map may contain:
+    - seq of annotation AST-maps
+    - seq of modifier keywords
+    - single type AST-map (or value)
+    - single string name
+
+   Annotations and type are also jenerated.
+   Modifiers are not validated, but must be keywords.
+   Name is not validated, so be sure to produce valid Java identifier."
   [jen-fn {:keys [annotations modifiers type name]}]
   {:pre [(string? name) (every? keyword? modifiers) (not (nil? type))]}
   (let [jen-annotatons (map jen-fn annotations)
