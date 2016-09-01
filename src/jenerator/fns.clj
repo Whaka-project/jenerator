fc6238a597d8cb2ae7b03333862764e6f52df43b
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2016-09-02 00:15:12 +0300

    #40 `fns` is majorly cleaned up. `type` fn is reused a lot.

--- src/jenerator/fns.clj
+++ src/jenerator/fns.clj
@@ -1,5 +1,22 @@
 (ns jenerator.fns
-  (:refer-clojure :exclude [int long float double type cast not]))
+  (:refer-clojure :exclude [int long float double type cast not])
+  (:require [jenerator.util :as u]))
+
+(defn- apply-if-v
+  "; (T* -> R) -> (A | Vec[T]) -> (A | R)
+   Takes a function and any element.
+   If element is a vector - applies the function to it and returns the result.
+   Otherwise returns the specified element.
+
+   Examples:
+     (apply-if-v inc 12) -> 12
+     (apply-if-v inc [12]) -> 13
+
+   This function is used by sugary-fns that allow the DSL-like approach to perform
+   complex or recursive calls. E.g. there's 'f' that may be called like this: (f 12 13)
+   So (f 12 [13 14]) may be equal to (f 12 (f 13 14))"
+  [f x]
+  (if (vector? x) (apply f x) x))
 
 (defn br
   "; Any -> Brackets-AST
@@ -39,13 +56,45 @@
   ([class arg-key arg-value & {:as other-args}]
     (ann class :args (assoc other-args arg-key arg-value))))
 
+(declare type-mapper)
+
 (defn type
+  "; (Class | Key) -> Type-AST
+   ; (Class | Key) -> Long -> Type-AST
+   Takes a type identifier (clall or a primitive type key) and optional array type depth.
+   Returns AST map representing a type descriptor.
+
+   Examples:
+     (type :int) -> AST for int type
+     (type String) -> AST for String type
+     (type String 2) -> AST for String[][] array type
+     (type Class) -> AST for raw Class type
+     (type [Class]) -> AST for diamond Class<> type
+     (type [Class Long]) -> AST for Class<Long> type
+     (type [Class [Class Long]] 1) -> AST for Class<Class<Long>>[] type
+
+   Vector calls are recursive, but array depth cannot ve specified that way.
+   So if you want to create a type with generic of array type, you have to use nested calls.
+   Example: (type [Class (type :long 1)]) -> AST for Class<long[]> type"
   ([types] (type types 0))
   ([types array]
-    (let [ftype (if (sequential? types) (first types) types)
-          generics (if (sequential? types) 
-                     (map #(if (or (class? %) (sequential? %)) (type %) %) (rest types)) nil)]
+    (let [is-vec (vector? types)
+          ftype (if is-vec (first types) types)
+          generics (if is-vec (map type-mapper (rest types)) nil)]
       {:jtag :type :type ftype :generics generics :array array})))
+
+(def type-identifier?
+  "; Any -> Boolean
+   Returns true if specified value may represent a type in the DSL."
+  (u/anyp keyword? class? vector?))
+
+(defn- type-mapper
+  "; (T | Type-Identifier) -> (T | Type-AST)
+   Takes any value, checks whether it's a `type-identifier',
+   and either applies `type' function to it, or returns value itself."
+  [value]
+  (if (type-identifier? value)
+    (type value) value))
 
 (defn cast
   "; Any -> (AST | Class | [Class])* -> AST
@@ -57,12 +106,11 @@
   [value & types]
   (reduce (fn [val type]
             {:jtag :cast :value val
-             :type (if (map? type) type
-                     (jenerator.fns/type type))})
+             :type (type-mapper type)})
           value types))
 
 (defn var
-  "; {Key -> Any}* -> Key* -> Class -> String -> {Key -> Any}
+  "; {Key -> Any}* -> Key* -> Type-Identifier -> String -> {Key -> Any}
    Weird varargs at the beginning of the argument-list denote
    that function actually expects only 2 last arguments - other are optional.
    Function takes 0+ annotation AST-maps, then 0+ modifier keywords,
@@ -78,10 +126,11 @@
   (let [[name type & stuff] (reverse rest)
         [modifiers annotations] (split-with keyword? stuff)]
     {:jtag :var :annotations (reverse annotations)
-     :modifiers (reverse modifiers) :type type :name name}))
+     :modifiers (reverse modifiers) :type (type-mapper type) :name name}))
 
 (defn decl
-  "; Var-AST -> Any -> ([String, Any]) -> Decl-AST
+  "; Var-AST -> Any -> Decl-AST
+   ; Var-AST -> Any -> ([String, Any]) -> Decl-AST
    Function takes a var AST map, a value, and optional seq of pairs of additional declarations.
    Returns AS map describing a variable declaration with initializers.
    If first argument is not a map - it's assumed that it's a seq of values,
@@ -97,7 +146,7 @@
    so it would be more convinient for callers to generate calls programatically."
   ([var value] (decl var value nil))
   ([var value values]
-   (let [var-ast (if (map? var) var (apply jenerator.fns/var var))]
+   (let [var-ast (apply-if-v jenerator.fns/var var)]
      {:jtag :decl :var var-ast :value value :values values})))
 
 (defn unary
@@ -120,14 +169,6 @@
    Takes a single value and produces AST for unary prefix negation (!)."
   [x]
   (unary '! (br x)))
-
-(defn- apply-if-v
-  "; (T* -> R) -> (A | Vec[T]) -> (A | R)
-   Takes a function and any element.
-   If element is a vector - applies the function to it and returns the result.
-   Otherwise returns the specified element."
-  [f x]
-  (if (vector? x) (apply f x) x))
 
 (defn bin
   "; Any -> Symbol -> Any -> Binary-AST
